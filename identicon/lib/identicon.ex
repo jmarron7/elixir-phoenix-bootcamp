7970a7931bb1048fe72088c7226fc62e5a0d00a6
Jesus Marron <jesusmarron7@gmail.com>
2023-09-19 13:46:06 -0500

    create pixel map

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -17,6 +17,8 @@ defmodule Identicon do
     |> hash_input
     |> pick_color
     |> build_grid
+    |> filter_odd_squares
+    |> build_pixel_map
   end
 
   def hash_input(input) do
@@ -25,6 +27,21 @@ defmodule Identicon do
       |> :binary.bin_to_list()
 
     %Identicon.Image{hex: hex}
+  end
+
+  def build_pixel_map(%Identicon.Image{grid: grid} = image) do
+    pixel_map =
+      Enum.map(grid, fn {_hex, index} ->
+        horizontal = rem(index, 5) * 50
+        vertical = div(index, 5) * 50
+
+        top_left = {horizontal, vertical}
+        bottom_right = {horizontal + 50, vertical + 50}
+
+        {top_left, bottom_right}
+      end)
+
+    %Identicon.Image{image | pixel_map: pixel_map}
   end
 
   def build_grid(%Identicon.Image{hex: hex} = image) do
@@ -41,6 +58,15 @@ defmodule Identicon do
   def mirror_row(row) do
     [first, second | _tail] = row
     row ++ [second, first]
+  end
+
+  def filter_odd_squares(%Identicon.Image{grid: grid} = image) do
+    grid =
+      Enum.filter(grid, fn {hex, _index} ->
+        rem(hex, 2) == 0
+      end)
+
+    %Identicon.Image{image | grid: grid}
   end
 
   def pick_color(image) do
