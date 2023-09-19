1dc739a38071413096af7fca65cff05e48167f46
Jesus Marron <jesusmarron7@gmail.com>
2023-09-19 14:11:49 -0500

    identicon finished

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -19,6 +19,8 @@ defmodule Identicon do
     |> build_grid
     |> filter_odd_squares
     |> build_pixel_map
+    |> draw_image
+    |> save_image(input)
   end
 
   def hash_input(input) do
@@ -53,6 +55,21 @@ defmodule Identicon do
       |> Enum.with_index()
 
     %Identicon.Image{image | grid: grid}
+  end
+
+  def draw_image(%Identicon.Image{color: color, pixel_map: pixel_map}) do
+    image = :egd.create(250, 250)
+    fill = :egd.color(color)
+
+    Enum.each(pixel_map, fn {top_left, bottom_right} ->
+      :egd.filledRectangle(image, top_left, bottom_right, fill)
+    end)
+
+    :egd.render(image)
+  end
+
+  def save_image(image, filename) do
+    File.write("#{filename}.png", image)
   end
 
   def mirror_row(row) do
